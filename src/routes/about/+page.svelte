5361ab97700e0f244164abea094fdd077e0af708
Kuiliang Zhang <kzhang48@students.cs.ubc.ca>
2025-03-01 23:55:16 -0800

    🐛 fix: 404 link

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { base } from '$app/paths';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcomeFallback from '$lib/images/svelte-welcome.png';
 </script>
@@ -31,11 +30,6 @@
 		The page you're looking at is purely static HTML, with no client-side interactivity needed.
 		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
 		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href={`${base}/sverdle`}>Sverdle</a> page illustrates SvelteKit's data loading and form handling.
-		Try using it with JavaScript disabled!
 	</p>
 </div>
 
