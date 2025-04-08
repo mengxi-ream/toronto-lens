a2233ea7f205683d4b4a5d4020a544f6c571ee3c
Kuiliang Zhang <z1219202167@outlook.com>
2025-04-08 11:41:04 -0700

    👷 ci: vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,12 +9,24 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.env.VITE_BUILD_GH_PAGES ? '/kzhang48/cpsc447-g15' : ''
-		}
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp'],
+				minimumCacheTTL: 300,
+				domains: ['example-app.vercel.app']
+			}
+		})
 	}
+
+	// config for static adapter
+	// kit: {
+	// 	adapter: adapter({
+	// 		fallback: '404.html'
+	// 	}),
+	// 	paths: {
+	// 		base: process.env.VITE_BUILD_GH_PAGES ? '/kzhang48/cpsc447-g15' : ''
+	// 	}
+	// }
 };
 
 export default config;
